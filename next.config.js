6fd5cbf1347220de8c343aa29f048f6433ad4816
nipun221 <2oics072@gbu.ac.in>
2023-09-13 11:57:39 +0530

    removed exp features from next.config.js and audit-fix

--- next.config.js
+++ next.config.js
@@ -3,10 +3,14 @@ const path = require('path')
 module.exports = {
   trailingSlash: true,
   reactStrictMode: false,
+
+  /*
   experimental: {
     esmExternals: false,
     jsconfigPaths: true // enables it for both jsconfig.json and tsconfig.json
   },
+  */
+
   webpack: config => {
     config.resolve.alias = {
       ...config.resolve.alias,
